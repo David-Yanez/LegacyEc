a63693b0e04db28c6be8b4c7a06319bc54a201fc
David-Yanez <luis.yanez@epn.edu.ec>
2024-11-20 22:07:24 -0500

    inicio del proyecto

---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,17 @@
+
+@extends('layout')
+
+@section('content')
+    <div class=" flex mt-14 bg-slate-500">
+        <div class=" flex flex-col gap-4">
+            <h1 class=" text-4xl text-center">Productos Naturales Legacy</h1>
+    
+            <p>Legacy te ofrece una variedad de bebidas naturales para cuidar tu cuerpo desde adentro. Nuestras bebidas naturales con colágeno, energía y fórmulas digestivas te ayudarán a alcanzar tus objetivos.</p>
+    
+        </div>
+        <figure>
+            <img src="/img/productosLegacy.webp" alt="banerLegacy">
+        </figure>
+    </div>
+   
+@endsection
